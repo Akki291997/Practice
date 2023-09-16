ab9b7eff15cbac881d1bc81ce6574bfcbd5e72e4
karan <karantompe49@gmail.com>
2023-09-16 14:36:50 +0530

    react

---
+++ test/src/componets/Start.js
@@ -0,0 +1,16 @@
+// import React from "react"
+// export const Start = () => {
+//   return <h1>Akash Kokare</h1>;
+// };
+
+// export const End = () => {
+//   return <h1>Akash </h1>;
+// };
+
+import React from "react";
+
+const Start = () => {
+  return <div>Start</div>;
+};
+
+export default Start;
